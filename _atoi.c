0f282512948a9d560fab951cd2c12d05f1bbc093
Augustinemary Agupugo <austinmary456@gmail.com>
2022-11-21 10:36:13 -0800

    Finally

--- _atoi.c
+++ _atoi.c
@@ -2,35 +2,33 @@
 
 /**
  * interactive - returns true if shell is interactive mode
- * @info: sruct address
+ * @info: struct address
  *
- * return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
-
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delin - checks if character is a delimeter
+ * is_delim - checks if character is a delimeter
  * @c: the char to check
- * @delin: the delimeter string
+ * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-
-int is_delin(char c, char *delin)
+int is_delim(char c, char *delim)
 {
-	while (*delin)
-		if (*delin++ == c)
+	while (*delim)
+		if (*delim++ == c)
 			return (1);
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
 
 int _isalpha(int c)
@@ -42,9 +40,9 @@ int _isalpha(int c)
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
 
 int _atoi(char *s)
@@ -52,7 +50,7 @@ int _atoi(char *s)
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
@@ -61,7 +59,7 @@ int _atoi(char *s)
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] -'0');
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
@@ -71,5 +69,6 @@ int _atoi(char *s)
 		output = -result;
 	else
 		output = result;
+
 	return (output);
 }
